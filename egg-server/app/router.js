3943f633c5f7c4f586f7c94eec99344e4b5a8761
east <573027166@qq.com>
2019-07-24 20:57:31 +0800

    .

--- egg-server/app/router.js
+++ egg-server/app/router.js
@@ -9,7 +9,6 @@ module.exports = app => {
   router.post('/upload', controller.upload.index);
   router.post('/user/add', controller.user.add);
   router.post('/goods/add', controller.goods.add); // 添加商品
-  router.get('/goods/query', controller.goods.query); // 查询商品信息
   router.get('/goods/list', controller.goods.list); // 获取商品列表
   router.get('/goods/del', controller.goods.del); // 删除商品信息
 };

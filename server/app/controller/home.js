952b30c3aed49dc623338cdbcc0c7d6c46970180
east <573027166@qq.com>
2019-08-01 11:26:44 +0800

    .

--- server/app/controller/home.js
+++ server/app/controller/home.js
@@ -1,20 +1,9 @@
 'use strict';
-const fs = require('fs')
-const path = require('path')
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    const {
-      ctx
-    } = this;
-    const tpl = path.join(__dirname, '../public/view/index.html');
-    const html = new Promise(function (resolve, reject) {
-      fs.readFile(tpl, 'utf-8', (err, data) => {
-        resolve(data)
-      });
-    })
-    ctx.body = await html
+    this.ctx.body = "hello"
   }
 }
 
